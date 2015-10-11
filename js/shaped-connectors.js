3144225efd2daa18a525f1a51cdab5a9f63842b2
Anthony <awmorp@gmail.com>
2015-10-11 18:42:54 +1100

    Refactored shaped connector code

---
+++ js/shaped-connectors.js
@@ -0,0 +1,46 @@
+/** Adds angled, rounded and brace-shaped connectors, and chooses connector shape based on type **/
+/* Author: Anthony Morphett, awmorp@gmail.com */
+
+/**
+ * SVG path for drawing a horizontal angled tab from top to bottom.
+ * @const
+ */
+Blockly.BlockSvg.TAB_PATH_DOWN_ANGLE = 'v 5 l -' + Blockly.BlockSvg.TAB_WIDTH + ',' + 7.5 +
+    ' l ' + Blockly.BlockSvg.TAB_WIDTH + ',' + 7.5;
+
+/**
+ * SVG path for drawing a horizontal round-shaped tab from top to bottom.
+ * @const
+ */
+Blockly.BlockSvg.TAB_PATH_DOWN_ROUND = 'v 5 a ' + Blockly.BlockSvg.TAB_WIDTH + ' ' + 7.5 +
+    ' 0 0 0 0 ' + 1.75*Blockly.BlockSvg.TAB_WIDTH;
+
+/**
+ * SVG path for drawing a horizontal brace-shaped tab from top to bottom.
+ * @const
+ */
+/*Blockly.BlockSvg.TAB_PATH_DOWN_BRACE = 'v 2.5 c -5,0 -2,9 -10,10 c 8,1 5,10 10,10'; */
+Blockly.BlockSvg.TAB_PATH_DOWN_BRACE = 'v 5 c -' + (0.5*Blockly.BlockSvg.TAB_WIDTH) + ',0' + 
+    ' -' + (0.2*Blockly.BlockSvg.TAB_WIDTH) + ',' + (0.9*7.5) +
+    ' -' + (0.75*Blockly.BlockSvg.TAB_WIDTH) + ',' + 7.5 +
+    ' c ' + (0.55*Blockly.BlockSvg.TAB_WIDTH) + ',' + (0.1*7.5) +
+    ' ' + (0.5*0.75*Blockly.BlockSvg.TAB_WIDTH) + ',' + (7.5) +  /* Not sure this is quite symmetric? */
+    ' ' + (0.75*Blockly.BlockSvg.TAB_WIDTH) + ',' + (7.5);
+
+
+/* Define new getConnector function.
+   Determine shape based on connector type.
+*/
+Blockly.BlockSvg.getConnectorPath = function(connector) {
+  var typeArray = connector.check_;
+  if( typeArray && typeArray.length == 1 ) {
+    switch( typeArray[0] ) {
+      case "Number": return( Blockly.BlockSvg.TAB_PATH_DOWN_ROUND );
+      case "Boolean": return( Blockly.BlockSvg.TAB_PATH_DOWN_ANGLE );
+      case "Set": return( Blockly.BlockSvg.TAB_PATH_DOWN_BRACE );
+      default: return( Blockly.BlockSvg.TAB_PATH_DOWN_PUZZLE );
+    }
+  } else {
+    return( Blockly.BlockSvg.TAB_PATH_DOWN_PUZZLE );
+  }
+}
