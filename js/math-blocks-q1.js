ba61aef3028b45e1349e34ecf63a6f2f25379193
Anthony <awmorp@gmail.com>
2015-10-26 08:51:31 +1100

    Polish up limit exercise

--- js/math-blocks-q1.js
+++ js/math-blocks-q1.js
@@ -8,15 +8,16 @@
 */
 
 
-var varlist = [[" ", "BLANK"],["ϵ", "epsilon"], ["M", "M"], ["δ", "delta"], ["x", "x"], ["n", "n"]];
-var valuelist = [[" ", "BLANK"],["0","0"],["L","L"],["ϵ", "epsilon"], ["M", "M"], ["δ", "delta"], ["x", "x"], ["n", "n"]]
-var setlist = [["ℝ", "REAL"], ["ℚ", "RATIONAL"], ["ℤ", "INTEGERS"], ["ℕ", "NATURALS"]];
+var varlist = [[" ", "BLANK"],["ϵ", "EPSILON"], ["δ", "DELTA"], ["L","L"], ["M", "M"], ["n", "n"], ["x", "x"]];
+var valuelist = [[" ", "BLANK"],["0","0"],["ϵ", "EPSILON"], ["δ", "DELTA"], ["L","L"], ["M", "M"], ["n", "n"], ["x", "x"]];
+var setlist = [[" ", "BLANK"], ["ℝ", "REAL"], ["ℚ", "RATIONAL"], ["ℤ", "INTEGERS"], ["ℕ", "NATURALS"]];
+var quantifierlist = [[" ", "BLANK"],["∀", "FORALL"],["∃","EXISTS"]];
 
 /****** Quantifiers ******/
-Blockly.Blocks['logic_forall_restricted'] = {
+Blockly.Blocks['logic_quantifier_set_restricted'] = {
   init: function() {
     this.appendValueInput("PREDICATE")
-        .appendField("∀")
+        .appendField(new Blockly.FieldDropdown(quantifierlist), "QUANTIFIER")
         .appendField(new Blockly.FieldDropdown(varlist), "VAR")
         .appendField("∈")
         .appendField(new Blockly.FieldDropdown(setlist), "SET")
@@ -29,10 +30,10 @@ Blockly.Blocks['logic_forall_restricted'] = {
   }
 };
 
-Blockly.Blocks['logic_forall_condition_restricted'] = {
+Blockly.Blocks['logic_quantifier_condition_restricted'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("∀")
+        .appendField(new Blockly.FieldDropdown(quantifierlist), "QUANTIFIER")
         .appendField(new Blockly.FieldDropdown(varlist), "VAR")
         .appendField(new Blockly.FieldDropdown([[">", ">"], ["≥", "≥"], ["<", "<"], ["≤", "v"], ["≠", "≠"]]), "COMPARISON_OPERATOR")
         .appendField(new Blockly.FieldDropdown(valuelist), "BOUND");
@@ -42,41 +43,6 @@ Blockly.Blocks['logic_forall_condition_restricted'] = {
     this.setOutput(true, "Boolean");
     this.setColour(booleanQuantifierHue);
     this.setTooltip('Universal (\'for all\') quantifier with condition');
-    this.setHelpUrl('http://www.example.com/');
-  }
-};
-
-Blockly.Blocks['logic_exists_restricted'] = {
-  init: function() {
-    this.appendValueInput("PREDICATE")
-        .appendField("∃")
-        .appendField(new Blockly.FieldDropdown(varlist), "VAR")
-        .appendField("∈")
-        .appendField(new Blockly.FieldDropdown(setlist), "SET")
-        .setCheck("Boolean");
-    this.setInputsInline(true);
-    this.setOutput(true, "Boolean");
-    this.setColour(booleanQuantifierHue);
-    this.setTooltip('Universal (\'for all\') quantifier');
-    this.setHelpUrl('http://www.example.com/');
-  }
-};
-
-/* https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#yt9arv */
-Blockly.Blocks['logic_exists_condition_restricted'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("∃")
-        .appendField(new Blockly.FieldDropdown(varlist), "VAR")
-        .appendField(new Blockly.FieldDropdown([[">", ">"], ["≥", "≥"], ["<", "<"], ["≤", "v"], ["≠", "≠"]]), "COMPARISON_OPERATOR")
-        .appendField(new Blockly.FieldDropdown(valuelist), "BOUND");
-    this.appendValueInput("PREDICATE")
-        .setCheck("Boolean")
-        .appendField(" s.t.");
-    this.setInputsInline(true);
-    this.setOutput(true, "Boolean");
-    this.setColour(booleanQuantifierHue);
-    this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
 };
