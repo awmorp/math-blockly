7e50eca74e37f8e94eac25c4919e89fa58d6f376
Anthony <awmorp@gmail.com>
2016-05-16 23:58:29 +1000

    Work on highlighting conflicting variables

--- js/math-blocks.js
+++ js/math-blocks.js
@@ -30,13 +30,16 @@ var setHue = Blockly.BlockSvg.NUMBER_COLOUR;
 Blockly.Blocks['logic_quantifier'] = {
   init: function() {
     var varField = new Blockly.FieldMathVariable("x", "Number", null, true);
-    varField.fieldCSSClassName = "blocklyEditableText blocklyQuantifierVarField"; /* Override CSS so that this field is displayed in number colour rather than boolean colour */
     this.appendDummyInput()
         .appendField(new Blockly.FieldDropdown([["∀", "∀"], ["∃", "∃"]],
             function(quantifier) { this.sourceBlock_.quantifierChanged_(quantifier) }), "QUANTIFIER")
         .appendField(varField, "VAR")
         .appendField(new Blockly.FieldDropdown([["∈","∈"],[">", ">"], ["≥", "≥"], ["<", "<"], ["≤", "≤"], ["≠", "≠"]],
             function(op) { this.sourceBlock_.operatorChanged_(op) }), "OPERATOR");
+    
+    /* Override CSS so that this field is displayed in number colour rather than boolean colour */
+    varField.addCSSClass( "blocklyQuantifierVarField" );
+    
     this.appendValueInput("SCOPE")
         .setCheck("Set")
         .parentVarsInScope_ = false;
